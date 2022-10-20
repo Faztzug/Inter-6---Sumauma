64da4f0cc7d8d1eecca08ad8476deebc3fceb925
guilhermeburgheri <guilherme.burgheri@hotmail.com>
2022-10-19 21:20:52 -0300

    Seleção de fases no menu inicial.

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -10,6 +10,24 @@ public class Menu : MonoBehaviour
         SceneManager.LoadScene("Fase 1");
     }
 
+    public void JogarFase1()
+    {
+        Debug.Log("Foi fase 1");
+        SceneManager.LoadScene("Fase 1");
+    }
+
+    public void JogarFase2()
+    {
+        Debug.Log("Foi fase 2");
+        SceneManager.LoadScene("Fase 2");
+    }
+
+    public void JogarFase3()
+    {
+        Debug.Log("Foi fase 3");
+        SceneManager.LoadScene("Fase 3");
+    }
+
     public void JogarNovamente()
     {
         GameState.ReloadScene(0);

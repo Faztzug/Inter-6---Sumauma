9a7581668bf35a5a91cc57c5ca164c6bdd90e372
Faztz <faztzug@gmail.com>
2022-11-27 01:49:24 -0300

    MUSICAS

--- Assets/Scripts/Sound/Sound.cs
+++ Assets/Scripts/Sound/Sound.cs
@@ -22,7 +22,14 @@ public class Sound
     public void Setup(AudioSource audioSource)
     {
         audioSource.clip = clip;
-        audioSource.volume = GameState.SaveData.mute ? 0f : volume * GameState.SaveData.sfxVolume;
+        if(GameState.GameStateInstance != null)
+        {
+            audioSource.volume = GameState.SaveData.mute ? 0f : volume * GameState.SaveData.sfxVolume;
+        }
+        else if(MenuInicial.MenuInicialInstance != null)
+        {
+            audioSource.volume = MenuInicial.SaveData.mute ? 0f : volume * MenuInicial.SaveData.sfxVolume;
+        }
         audioSource.pitch = pitch;
         audioSource.loop = loop;
         audioSource.playOnAwake = playOnAwake;
@@ -30,7 +37,9 @@ public class Sound
 
     public void PlayOn(AudioSource audioSource)
     {
-        if(GameState.SaveData.mute) return;
+        if(GameState.GameStateInstance != null && GameState.SaveData.mute) return;
+        if(MenuInicial.MenuInicialInstance != null && MenuInicial.SaveData.mute) return;
+        
         Setup(audioSource);
         if(pitchRNG.Length >= 2 && pitchRNG[0] != pitchRNG[1])
         {

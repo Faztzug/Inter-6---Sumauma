291e8190dc65678425f083fe3f0965c7d63f4400
DESKTOP-SPAB7KT\windows <vitorlima.0811@gmail.com>
2022-12-02 01:01:28 -0300

    Agora tem alminhas em todas as fases

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -6,11 +6,17 @@ public class Item : MonoBehaviour
 { 
     [SerializeField] protected int ammount;
     [SerializeField] protected Sound collectSound;
+    public GameObject alma;
 
     protected virtual void Start() { }
     void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player")) CollectItem(other);
+        if (other.CompareTag("Player"))
+        {
+            CollectItem(other);
+            alma.SetActive(true);
+        }
+
     }
 
     public virtual void CollectItem(Collider info)
